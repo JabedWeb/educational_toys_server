c09ae0f1662b230b2b039107c47821ddd94d42b7
jabed366 <jabedhasan231@gmail.com>
2023-05-20 04:53:32 +0600

    vercel client connect

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ const express =require('express');
 const app = express();
 const cors=require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const e = require('express');
 //env port or 5000 port
-const PORT = process.env.PORT || 5000;
+const PORT =  5000;
 
 //middleware
+
 app.use(cors());
+
 app.use(express.json());
 
 require('dotenv').config();
 
 //mongodb connection
-//const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lkb5wuy.mongodb.net/?retryWrites=true&w=majority`;
-const uri ='mongodb://localhost:27017';
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lkb5wuy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -21,13 +23,22 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  maxPoolSize: 10,
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+   await client.connect((err) => {
+      if (err) {
+        console.log(err);
+        return;
+        } 
+    });
 
 
     // find all databases
@@ -57,15 +68,12 @@ async function run() {
         query={sellerEmail:req.query.email}
       }
      const result= await educational_toys.find(query).toArray();
-     console.log(result);
     res.send(result);
     })
     
      //post data
     app.post('/toys',async(req,res)=>{
-      console.log("okkkk");
       const newToy=req.body;
-      console.log('adding new toy',newToy);
         const result= await educational_toys.insertOne(newToy);
         res.json(result);
     })
@@ -75,7 +83,6 @@ async function run() {
     // my toys delete
     app.delete('/toys/:id',async(req,res)=>{
       const id=req.params.id;
-      console.log(id);
       const ids =new ObjectId(id);
       const result= await educational_toys.deleteOne({_id:ids});
       res.send(result);
@@ -84,18 +91,15 @@ async function run() {
     //update data
     app.put('/toys/:id',async(req,res)=>{
       const id=req.params.id;
-      console.log(id);
       const ids =new ObjectId(id);
       const updatedData=req.body;
       const options={upsert:true};
-      console.log(updatedData);
       const result= await educational_toys.updateOne({_id:ids},{$set:updatedData},options);
       res.send(result);
     })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -104,11 +108,10 @@ async function run() {
 run().catch(console.dir);
 
 
-//route get
-app.get('/',(req,res)=>{
-    res.send('Hello World');
-})
 
+app.get('/',(req,res)=>{
+  res.send('Running Educational Toys Server');
+})
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
 })
